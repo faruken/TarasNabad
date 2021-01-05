2ccfbc7e1f6c674cc1dd0ecae74fba66b37c22bf
faruken <faruken@users.noreply.github.com>
2021-01-05 20:17:44 +1100

    Add threadpool

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ extern crate tiny_http;
 use tiny_http::{Server, Response, Method};
 use crate::taras::{TarasNabadImpl, TarasNabad};
 use uuid::Uuid;
-use url::Url;
-use std::str::FromStr;
+use rocksdb::Error;
+use structopt::StructOpt;
+use futures::executor::{ThreadPoolBuilder, ThreadPool};
+use futures::task::SpawnExt;
+use futures::future;
 
 
 fn is_json(request: &tiny_http::Request) -> bool {
@@ -20,59 +23,99 @@ fn is_json(request: &tiny_http::Request) -> bool {
 }
 
 
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(short = "p", long = "port", default_value = "8000")]
+    port: u16,
+
+    #[structopt(short = "t", long = "thread-pool-size", default_value = "32")]
+    thread_pool_size: usize,
+}
+
+
 fn main() {
-    let mut db = taras::TarasNabad::new("d.db");
-    let server = Server::http("127.0.0.1:8000").unwrap();
+    let opt = Opt::from_args();
+    let port = opt.port;
+    let thread_pool_size = opt.thread_pool_size;
+    let pool = ThreadPoolBuilder::new().pool_size(thread_pool_size).create().expect("Failed to create thread pool");
+    let mut db = taras::TarasNabad::new("d.db").to_owned();
+    let server = Server::http(("127.0.0.1", port)).unwrap();
     println!("Welcome home, great slayer.");
-    println!("Serving on 127.0.0.1:8000");
-    for mut request in server.incoming_requests() {
-        match request.method() {
-            &Method::Get => {
-                let uri = request.url().trim();
-                let path = &uri[1..];
-                match Uuid::parse_str(path) {
-                    Ok(value) => {
-                        println!("received get {}", path);
-                        match db.get(&value) {
-                            Ok(Some(data)) => {
-                                request.respond(Response::from_string(String::from_utf8(data).unwrap()));
-                            }
-                            Ok(None) => {
-                                let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(404));
-                                let _ = request.respond(rep);
-                            }
-                            Err(_e) => {
-                                let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(500));
-                                let _ = request.respond(rep);
+    println!("Serving on 127.0.0.1:{}", port);
+    futures::executor::block_on(future::lazy(move |_| {
+        for mut request in server.incoming_requests() {
+            let mut db = db.clone();
+            match request.method() {
+                &Method::Get => {
+                    let uri = request.url().trim();
+                    let path = &uri[1..];
+                    match Uuid::parse_str(path) {
+                        Ok(value) => {
+                            match db.get(&value) {
+                                Ok(Some(data)) => {
+                                    pool.spawn(future::lazy(move |_| {
+                                        request.respond(Response::from_string(String::from_utf8(data).unwrap()));
+                                    })).unwrap();
+                                }
+                                Ok(None) => {
+                                    let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(404));
+                                    let _ = request.respond(rep);
+                                }
+                                Err(_e) => {
+                                    let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(500));
+                                    let _ = request.respond(rep);
+                                }
                             }
                         }
+                        Err(e) => {
+                            let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(404));
+                            let _ = request.respond(rep);
+                        }
                     }
-                    Err(e) => {
-                        let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(404));
+                }
+                &Method::Put => {
+                    if !is_json(&request) {
+                        let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(400));
                         let _ = request.respond(rep);
+                    } else {
+                        pool.spawn(future::lazy(move |_| {
+                            let mut content = String::new();
+                            request.as_reader().read_to_string(&mut content).unwrap();
+                            let data: String = content.parse().unwrap();
+                            let flag = db.put(data).to_string();
+                            let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(201));
+                            request.respond(Response::from_string(flag));
+                        })).unwrap();
                     }
                 }
-            }
-            &Method::Put => {
-                println!("received put");
-                if !is_json(&request) {
-                    let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(400));
-                    let _ = request.respond(rep);
-                } else {
-                    let mut content = String::new();
-                    request.as_reader().read_to_string(&mut content).unwrap();
-                    let data: String = content.parse().unwrap();
-                    let flag = db.put(data).to_string();
-                    let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(201));
-                    request.respond(Response::from_string(flag));
+                &Method::Delete => {
+                    let uri = request.url().trim();
+                    let path = &uri[1..];
+                    match Uuid::parse_str(path) {
+                        Ok(value) => {
+                            match db.delete(&value) {
+                                Ok(()) => {
+                                    pool.spawn(future::lazy(move |_| {
+                                        let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(200));
+                                        let _ = request.respond(rep);
+                                    })).unwrap();
+                                }
+                                Err(_e) => {
+                                    let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(500));
+                                    let _ = request.respond(rep);
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            let rep = tiny_http::Response::new_empty(tiny_http::StatusCode(404));
+                            let _ = request.respond(rep);
+                        }
+                    }
                 }
-            }
-            &Method::Delete => {
-                println!("received delete");
-            }
-            _ => {
-                request.respond(Response::from_string("Unsupported Method"));
+                _ => {
+                    request.respond(Response::from_string("Unsupported Method"));
+                }
             }
         }
-    }
+    }));
 }
